c79abbefccd7573b0a13f7706312abba9d6d0e6a
ligen <ligen@meizu.com>
2020-07-24 15:36:44 +0800

    fix convert error

--- lutPNG2Cube.py
+++ lutPNG2Cube.py
@@ -9,19 +9,21 @@ def lutPNG2Cube(pngFile):
     fo = open(pngFile.split(".")[0]+".cube", "w")
     fo.writelines("LUT_3D_SIZE " + str(LUT_3D_SIZE) + "\n")
 
+    totalLine = 0
     for z in range(LUT_3D_SIZE):
-        startX = LUT_3D_SIZE * (z % 8)
-        startY = LUT_3D_SIZE * (z / 8)
+        startX = LUT_3D_SIZE * (int(z % 8))
+        startY = LUT_3D_SIZE * (int(z / 8))
         for y in range(LUT_3D_SIZE):
             for x in range(LUT_3D_SIZE):
-                if len(img_array[x + startX, +startY])==4:
-                    r, g, b, a = img_array[x + startX, +startY]
+                if len(img_array[x + startX, y + startY])==4:
+                    r, g, b, a = img_array[x + startX, y + startY]
                 else:
-                    r, g, b = img_array[x + startX, +startY]
+                    r, g, b = img_array[x + startX, y + startY]
                 fo.writelines(
-                    str(round(r / 255, 6)) + " " + str(round(g / 255, 6)) + " " + str(round(b / 255, 6)) + "\n")
+                    str(round(float(r / 255), 9)) + " " + str(round(float(g / 255), 9)) + " " + str(round(float(b / 255), 9)) + "\n")
+                totalLine = totalLine+1
     fo.close()
-    print("Done, "+ pngFile)
+    print("Done, "+ pngFile+"  total line:"+str(totalLine))
 
 
 lutPNG2Cube("test_lut.png")
